4d67bd9f56dd073026091a9183eac1bd111e34e8
Bibash Adhikari <bibash1270@gmail.com>
2019-06-01 19:57:20 +0545

    Add genesis block

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -67,7 +67,7 @@ TurtleCoind --print-genesis-tx --genesis-block-reward-address TRTLv2Fyavy8CXG8BP
 * You should see your premine appear in the previously generated wallet.
 
 */
-const char     GENESIS_COINBASE_TX_HEX[]                     = "010a01ff000188f3b501029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210142694232c5b04151d9e4c27d31ec7a68ea568b19488cfcb422659a07a0e44dd5";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "011401ff000100024634ddd12e7872572695f591e46ed6f3b072090b57b62144239c87370edc5b0a2101415c2f0ec1653eec39d10b00a9240d6351e6ca2d18b3f937f80ce8c7aa4c9a64";
 static_assert(sizeof(GENESIS_COINBASE_TX_HEX)/sizeof(*GENESIS_COINBASE_TX_HEX) != 1, "GENESIS_COINBASE_TX_HEX must not be empty.");
 
 /* This is the unix timestamp of the first "mined" block (technically block 2, not the genesis block)
